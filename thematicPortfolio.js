554ccc753feaff73633e2718e77d755707c3b394
blacktangolondon <blacktangocapital@gmail.com>
2025-06-04 15:04:02 +0200

    Update thematicPortfolio.js

--- thematicPortfolio.js
+++ thematicPortfolio.js
@@ -124,7 +124,6 @@ function loadThematicPortfolio() {
   </div>
   <div class="thematic-portfolio-contents">
     <div class="portfolio-tab-content active" data-category="stocks">
-      ${renderSection('Value Investing',        ['Instrument','P/E','P/B','Div Yield','Score','Trend','Approach','Gap to Peak','Key Area'], valueStocks)}
       ${renderSection('Low S&P500 Correlation', ['Instrument','Score','Correlation','Trend','Approach','Gap to Peak','Key Area'], stk2)}
       ${renderSection('Low Volatility',         ['Instrument','Score','Volatility','Trend','Approach','Gap to Peak','Key Area'], stk3)}
       ${renderSection('Trend Plus',             ['Instrument','Score','Bullish Alpha','Bearish Alpha','Alpha Strength','Trend','Approach','Gap to Peak','Key Area'], stk4)}
