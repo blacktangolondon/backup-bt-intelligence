a2baccec6e7759ad9b2b1285f66b4e5cdeb157f4
blacktangolondon <blacktangocapital@gmail.com>
2025-07-29 12:10:17 +0100

    Update spreadView.js

--- spreadView.js
+++ spreadView.js
@@ -212,7 +212,7 @@ const LABEL_MAP = {
   'BA/LMT'               : 'Boeing / Lockheed Martin',
   'V/MA'                 : 'Visa / Mastercard',
   'NFLX/DIS'             : 'Netflix / Disney',
-  'BAT.L/IMB.L'             : 'British American Tobacco / Imperial Brands',
+  'F/GM'                 : 'Ford / General Motors',
 
   // ── FIXED INCOME
   'ZN/ZF'                : '10Y Note / 5Y Note',
